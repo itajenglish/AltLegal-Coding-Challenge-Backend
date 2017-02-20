42d5f0ec196a106f757970cb773bbd8417840d66
Taj English <tajenglish@gmail.com>
2017-02-20 03:00:29 -0500

    Added Twitter Route

--- routes/watchlist.js
+++ routes/watchlist.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const Twitter = require('twitter-node-client').Twitter;
+const twitter = new Twitter(require('../lib/config/twitter_config'));
 const { getAllWatchlist, addNewWatchlist, getOneWatchList } = require('../models/watchlist');
 
 // Route Returns All Watchlist
@@ -12,5 +14,22 @@ router.post('/watchlist', addNewWatchlist, (req, res, next) => {
 
 // Route Returns Watchlist With ID Passed In
 router.get('/watchlist/:id', getOneWatchList, (req, res, next) => {});
+
+// Route Returns Tweets With Hashtag Passed In
+router.get('/tweets/:hashtag', (req, res, next) => {
+  const hashtag = req.params.hashtag
+  //Callback functions for twitter
+  const error = (err, response, body) => {
+    console.log(`error: ${err}`);
+    res.status(500).send('oh Ohh! something went wrong with adding your tweets.');
+  };
+  const success = (data) => {
+    console.log(data);
+    res.send(data);
+  };
+  //Hit Twitter Api
+  twitter.getSearch({'q':`${hashtag}`,'count': 10, 'lang': 'en'}, error, success);
+
+});
 
 module.exports = router;
