eb56c017e53812b5b61814152a863b7b0b394550
Taj English <tajenglish@gmail.com>
2017-02-21 09:28:49 -0500

    Fixed PORT conflict with React App

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bdPars.urlencoded({
 
 
 //start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running at: http://localhost:${PORT}/`);
 });
